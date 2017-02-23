ffa16a59f070e6d67b254aa7c031b20ecfc0d053
VanHakobyan <van19962013@mail.ru>
2017-02-23 23:46:59 +0400

    Add upper

---
+++ HomeWork001/Upper case/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Upper_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string st = File.ReadAllText(@"D:\text.txt");
+            Console.WriteLine("all text");
+            Console.WriteLine();
+            Console.WriteLine(st);
+            Console.WriteLine(new string('-',30));
+            Console.WriteLine("Only Upper");
+            Console.WriteLine();
+            for (int i = 0; i < st.Length; i++)
+            {
+                if (st[i] >= 'A' && st[i] <= 'Z')
+                    Console.Write(st[i]+" ");
+            }
+        }
+    }
+}
