2b18ff397e3810fb80dd39d659f0f08d73225830
VanHakobyan <van19962013@mail.ru>
2017-03-10 00:46:53 +0400

    add tree directory

---
+++ HomeWork003/Folder/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+
+namespace Folder
+{
+    public class RecursiveFileSearch
+    {
+        static void Main()
+        {
+            try
+            {
+                DirectoryInfo s = new DirectoryInfo(Console.ReadLine());
+                WalkDirectoryTree(s);
+            }
+            catch (DirectoryNotFoundException d)
+            {
+                Console.WriteLine(d.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+        }
+
+        static void WalkDirectoryTree(DirectoryInfo root)
+        {
+            FileInfo[] files = null;
+            DirectoryInfo[] subDirs = null;
+
+            try
+            {
+                files = root.GetFiles("*.*");
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+           
+
+            if (files != null)
+            {
+                foreach (FileInfo fi in files)
+                {
+                    Console.WriteLine(fi.DirectoryName);
+                    Console.WriteLine(fi.FullName);
+                    Console.WriteLine(fi.CreationTime);
+                }
+                subDirs = root.GetDirectories();
+
+                foreach (DirectoryInfo dirInfo in subDirs)
+                {
+                    WalkDirectoryTree(dirInfo);
+                }
+            }
+        }
+    }
+}
