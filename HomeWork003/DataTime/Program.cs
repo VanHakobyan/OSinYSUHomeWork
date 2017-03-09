e9c848200023625554dc613419866d4236afde8f
VanHakobyan <van19962013@mail.ru>
2017-03-10 00:02:25 +0400

    add DataTime

---
+++ HomeWork003/DataTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace _DataTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data and Time now - " + DateTime.Now);
+            string path = Console.ReadLine();
+            Console.WriteLine("Last Access Time  - " + File.GetLastAccessTimeUtc(@path));
+            Console.WriteLine("changed date and time stile now");
+            File.SetCreationTime(@path, DateTime.Now);
+            Thread.Sleep(1000);
+            Console.WriteLine(File.GetLastAccessTimeUtc(@path));
+
+        }
+    }
+}
