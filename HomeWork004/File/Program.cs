84dd1a761ceac0021f46bdf57a17865a3e8dc515
VanHakobyan <van19962013@mail.ru>
2017-03-16 19:52:27 +0400

    add DesktopCleaner

--- HomeWork004/File/Program.cs
+++ HomeWork004/File/Program.cs
@@ -1,17 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
-namespace FileMan
+namespace DesktopCleaner
 {
-    class Program
+
+    public static class CleanMaster
     {
+        static string desktopPath = string.Empty;
+
+        public static void Desktop()
+        {
+            desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            foreach (var filr in Directory.GetFiles(desktopPath))
+                if (filr.Split('.')[filr.Split('.').Length - 1] != "exe")
+                    File.Delete(filr);
+            Console.WriteLine("File Deleted!");
+        }
+        public static void DeleteFrom(string path, string suffix)
+        {
+            foreach (var f in Directory.GetFiles(path))
+                if (f.Split('.')[f.Split('.').Length - 1] == suffix)
+                    File.Delete(f);
+            Console.WriteLine($"The .{suffix} files are deleted from {path}");
+        }
         static void Main(string[] args)
         {
-            
+            //Desktop();
+            string path = @"C:\Users\Van\Desktop\new";
+            DeleteFrom(path, "bmp");
         }
     }
 }
