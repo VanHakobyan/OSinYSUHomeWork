91fb9c3fd11de8231185ad2095c25868c8453876
VanHakobyan <van19962013@mail.ru>
2017-04-06 22:20:02 +0400

    .txt file in d:\\007

---
+++ HomeWork005/HomeWork005/Program.cs
@@ -0,0 +1,14 @@
+﻿//Մուտքագրել թղթապանակի անուն, այն դարձել ընթացիկ և 
+//պատճենել նրա միջի բոլոր.txt ֆայլերը d:\\007 թղթապանակի մեջ։
+
+using System;
+using System.IO;
+namespace HomeWork005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
